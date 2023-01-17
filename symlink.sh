4d3a00e61a2a70c554ca2dc1329e9ad6b45936ec
IsSay <issay.4el@gmail.com>
2023-01-17 21:02:36 +0500

    new hotkeys

---
+++ symlink.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+BASE_DIR=/data/Documents/dotfiles
+
+mkdir ~/.config
+
+rm -rfv ~/.config/bspwm
+ln -sfv $BASE_DIR/bspwm ~/.config/bspwm
+
+rm -rfv ~/.config/picom
+ln -sfv $BASE_DIR/picom ~/.config/picom
+
+rm -rfv ~/.config/polybar
+ln -sfv $BASE_DIR/polybar ~/.config/polybar
+
+rm -rfv ~/.config/rofi
+ln -sfv $BASE_DIR/rofi ~/.config/rofi
+
+rm -rfv ~/.config/sxhkd
+ln -sfv $BASE_DIR/sxhkd ~/.config/sxhkd
+
+# chmod +x ~/.config/bspwm/bspwmrc
+# chmod +x ~/.config/sxhkd/sxhkdrc
