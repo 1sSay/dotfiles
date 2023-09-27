222e47ddec8ad18b762469e1aabe709806373c30
1sSay <issay.4el@gmail.com>
2023-09-27 21:52:40 +0000

    symlink update

--- symlink.sh
+++ symlink.sh
@@ -9,3 +9,9 @@ ln -sfv $BASE_DIR/hypr ~/.config/hypr
 
 rm -rfv ~/.config/waybar
 ln -sfv $BASE_DIR/waybar ~/.config/waybar
+
+rm -rfv ~/.config/kitty
+ln -sfv $BASE_DIR/kitty ~/.config/kitty
+
+rm ~/.zshrc
+ln -sfv $BASE_DIR/.zshrc ~/.zshrc
